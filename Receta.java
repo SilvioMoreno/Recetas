2e481da663b658c02dc6a9ade79a7af1d131e06c
Silvio Moreno <silviko98@gmail.com>
2017-05-31 12:01:47 +0200

    Añadidos los getters y setters
    Signed-off-by: Silvio Moreno <silviko98@gmail.com>

--- Receta.java
+++ Receta.java
@@ -3,6 +3,36 @@ import java.util.*;
 public class Receta 
 {
 
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getIngredientes() {
+		return ingredientes;
+	}
+	public void setIngredientes(String ingredientes) {
+		this.ingredientes = ingredientes;
+	}
+	public String getPreparacion() {
+		return preparacion;
+	}
+	public void setPreparacion(String preparacion) {
+		this.preparacion = preparacion;
+	}
+	public ArrayList<String> getConstructor1() {
+		return Constructor1;
+	}
+	public void setConstructor1(ArrayList<String> constructor1) {
+		Constructor1 = constructor1;
+	}
+	public ArrayList<String> getConstructor2() {
+		return Constructor2;
+	}
+	public void setConstructor2(ArrayList<String> constructor2) {
+		Constructor2 = constructor2;
+	}
 	String nombre;
 	String ingredientes;
 	String preparacion;
