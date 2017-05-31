de3f9313ba873e621ced00eb2fbd79bac3a16cf2
Silvio Moreno <silviko98@gmail.com>
2017-05-31 12:13:52 +0200

    Añadidos getters y setters a la clase Ingredientes.
    Signed-off-by: Silvio Moreno <silviko98@gmail.com>

--- Receta.java
+++ Receta.java
@@ -43,6 +43,18 @@ public class Receta
 	
 	public class Ingredientes
 	{
+		public String getCantidad() {
+			return cantidad;
+		}
+		public void setCantidad(String cantidad) {
+			this.cantidad = cantidad;
+		}
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
 		String cantidad;
 		String nombre;
 		
